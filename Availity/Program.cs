6d7939faa2386d228dbae93391cbea686a8e1957
hpaudel <h2paudel@gmail.com>
2022-08-10 14:59:27 -0400

    Adding project to git

---
+++ Availity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Availity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
