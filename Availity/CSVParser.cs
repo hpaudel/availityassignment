ef07528cda8390dad41e28ab6c421522faf8eaba
hpaudel <h2paudel@gmail.com>
2022-08-11 15:10:10 -0400

    Creating user object

--- Availity/CSVParser.cs
+++ Availity/CSVParser.cs
@@ -10,14 +10,30 @@ namespace Availity
         {
             try
             {
-                Console.WriteLine("CSV file parsed");
+                
+
+                IList<User> userList = new List<User>();
                 
                 for (int i=1;i<csvString.Length;i++)
                 {
-                    Console.WriteLine($" {} {} {} {}");
+                    
+                    string[] values = csvString[i].Split(',');
+
+                    User user = new User
+                    {
+                        UserId = Convert.ToInt32(values[0]),
+                        FirstName = values[1],
+                        LastName = values[2],
+                        Version = Convert.ToInt32(values[3]),
+                        Insurance = values[4]
+                    };
+
+                    userList.Add(user);
+                    
                 }
-                
-                
+
+                Console.WriteLine(userList);
+
             }
             catch (Exception)
             {
---
+++ Availity/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Availity
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Version { get; set; }
+        public string Insurance { get; set; }
+    }
+}
